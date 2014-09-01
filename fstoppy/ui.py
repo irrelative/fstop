7b96ec985b01e9bf6e725153d96a2b2e36f69f85
Justin Davis <justin@chartio.com>
2014-09-01 00:07:14 -0700

    ui generating svg

--- fstoppy/ui.py
+++ fstoppy/ui.py
@@ -1,5 +1,38 @@
+import subprocess
+import tempfile
+
 import jinja2
 import web
+
+import base
+
+
+def gen_svg(model):
+    nodes = []
+    vertices = []
+    for s in model.stocks:
+        nodes.append(s.name)
+    for f in model.flows:
+        nodes.append('%s [style=invis]' % f.name)
+        if f.to:
+            vertices.append((f.name, f.to.name, f.name))
+        if f.from_:
+            vertices.append((f.from_.name, f.name, f.name))
+    nodes = ';\n'.join(nodes)
+    vertices = ';\n'.join(['%s->%s [label="%s"]' % v for v in vertices])
+
+    with tempfile.NamedTemporaryFile(delete=False) as tf_input, tempfile.NamedTemporaryFile() as tf_output:
+        tf_input.write('''digraph unix {
+size="6,6";
+%s
+%s
+}
+        ''' % (nodes, vertices))
+        tf_input.close()
+        p = subprocess.Popen(['dot', '-Tsvg', tf_input.name, '-o', tf_output.name])
+        p.wait()
+        with open(tf_output.name) as f:
+            return f.read()
 
 
 class Handler(object):
@@ -13,11 +46,26 @@ class Handler(object):
 class Index(Handler):
 
     def GET(self):
-        return self.render('index.html', {})
+        model = base.Model(96)
+        s = base.Stock('account', 2000)
+        model.stocks.append(s)
+        f = base.Flow('deposit', '1000', to=s)
+        model.flows.append(f)
+        f = base.Flow('interest', 'account * .008', to=s)
+        model.flows.append(f)
+        svg = gen_svg(model)
+        return self.render('index.html', {'svg': svg})
+
+
+class About(Handler):
+
+    def GET(self):
+        return self.render('about.html', {})
 
 
 urls = (
     '/', Index,
+    '/about', About,
 )
 
 app = web.application(urls, globals())
